c048d7ffde3185ac0d5fec6c92bffba996c2dbdb
happineer <potato1291@gmail.com>
2018-06-25 00:37:53 +0900

    tr:opt10026 implement

--- kiwoom/tr_post.py
+++ kiwoom/tr_post.py
@@ -2,6 +2,7 @@
 class PostFn():
     def __init__(self, tr_mgr):
         self.fn_table = {
+            "고저PER": tr_mgr.post_opt10026,
             "주식분봉": tr_mgr.post_opt10080,
             "주식일봉": tr_mgr.post_opt10081,
             "주식주봉": tr_mgr.post_opt10082,
