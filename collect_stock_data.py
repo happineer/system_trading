043b3d13078a57357e98e8b7f8bee281aa040a9e
toptrader <potato1291@gmail.com>
2018-07-10 01:54:26 +0900

    improve collecting stock data app

--- collect_stock_data.py
+++ collect_stock_data.py
@@ -4,17 +4,12 @@ import pdb
 import sys
 import time
 from pymongo import MongoClient
-from slacker import Slacker
 from config import config_manager
 import datetime
+from util.tt_slack import TTSlack
 
 
-def print_n_noti_slack(slack, msg):
-    print(msg)
-    slack.chat.post_message('#general', msg)
-
-
-def delay(t):
+def delay_min(t):
     for curr_time in range(t)[::-1]:
         print("delay(min) -> %s / %s" % (curr_time, t))
         time.sleep(60)
@@ -23,11 +18,13 @@ def delay(t):
 def main(duration_list):
     mongo = MongoClient()
     db = mongo.TopTrader
-    end_date = datetime.datetime(2018, 7, 4, 16, 0, 0)
-    slack = Slacker(config_manager.get_slack_token())
+    with open("collect_stock_data_last_date.txt") as f:
+        date_str = f.read().strip().split(" ")
+    end_date = datetime.datetime(*date_str)
+    t_slack = TTSlack()
 
     for duration in duration_list:
-        print_n_noti_slack(slack, "[Automation] Start to collect stock data -> {}".format(duration))
+        t_slack.notify("[Automation] Start to collect stock data -> {}".format(duration))
 
         while True:
             cmd = "python collect_stock_data_time_unit.py {}".format(duration)
@@ -37,13 +34,15 @@ def main(duration_list):
             ret = error_status['error_code']
             print("ret_code:{} -> python collect_stock_data_time_unit.py {}".format(ret, duration))
             if ret == -100:  # kiwoom server check time (mon~sat)
-                print("Delay 15 minutes due to Kiwoom server check time. Mon~Sat")
-                print("Until : {}".format(datetime.datetime.today() + datetime.timedelta(minutes=10)))
-                delay(2)
+                delay = 20
+                print("Delay {} minutes due to Kiwoom server check time. Mon~Sat".format(delay))
+                print("Until : {}".format(datetime.datetime.today() + datetime.timedelta(minutes=delay)))
+                delay_min(delay)
             elif ret == -101:  # kiwoom server check time (mon~sat)
-                print("Delay 40 minutes due to Kiwoom server check time. Sun")
-                print("Until : {}".format(datetime.datetime.today() + datetime.timedelta(minutes=40)))
-                delay(40)
+                delay = 45
+                print("Delay {} minutes due to Kiwoom server check time. Sun".format(delay))
+                print("Until : {}".format(datetime.datetime.today() + datetime.timedelta(minutes=delay)))
+                delay_min(delay)
 
             cur = db.time_series_temp.find({'type': duration})
             if cur.count() == 0:  # exception case.. retry
@@ -53,18 +52,17 @@ def main(duration_list):
             if status['last'] == (status['total']-1) and status['end_date'] == end_date:
                 print("Collecting & Storing Stock {} Data is completed!".format(duration))
                 break
-        print_n_noti_slack(slack, "[Automation][{}] Complete to collect & save data...".format(duration))
+        t_slack.notify("[Automation] Complete to collect stock data -> {}".format(duration))
 
 
 if __name__ == "__main__":
-    # main(sys.argv[1:])
-    main(["min1",
-          "min3",
-          "min5",
-          "min10",
-          "min60",
-          "day",
-          "week",
-          "month",
-          "year",
-          ])
+    main([
+        "min1",
+        "min3",
+        "min5",
+        "min10",
+        "min60",
+        "day",
+        "week",
+        "month"
+    ])
