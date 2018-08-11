22dfbc96ca085e5de68641172839c4384d8d9278
toptrader <potato1291@gmail.com>
2018-08-12 08:16:32 +0900

    feature: trading history GH #24
    - gen snapshot at trading moment and record it

--- trading/account.py
+++ trading/account.py
@@ -1,11 +1,12 @@
 import pdb
-
 from singleton_decorator import singleton
 
 from config import config_manager as cfg_mgr
+from database.db_manager import DBM
 from trading.stock import Stock
 from util.tt_logger import TTlog
 from util import common
+
 
 @singleton
 class Account(object):
@@ -15,16 +16,19 @@ class Account(object):
     -
 
     """
-    def __init__(self, 잔고=10000000):
-        self.시작잔고 = 잔고
-        self.예수금 = 잔고
+    def __init__(self, init_balance, th):
+        self.시작잔고 = init_balance
+        self.예수금 = init_balance
         self.총평가금액 = 0.0
-        self.총매입금액 = 0.0
+        self.총매보입금액 = 0.0
         self.총손익 = 0.0
-        self.추정자산 = 잔고
+        self.추정자산 = init_balance
         self.총수익률 = 0.0
         self.실현손익 = 0.0
         self.보유주식 = {}
+        self.dbm = DBM('TopTrader')
+        self.timestamp = None
+        self.trading_history = th
         self.logger = TTlog().logger
 
     def print_attr(self, trading_type):
@@ -70,16 +74,53 @@ class Account(object):
         self.총수익률 = float(self.총손익) / self.시작잔고 * 100
         if stock.보유수량 == 0:
             del self.보유주식[stock.code]
-
         else:
             self.보유주식[stock.code] = stock
 
+        self.sell_transaction(stock)
+
         try:
             self.print_attr('sell')
-        except:
+        except Exception:
             pdb.set_trace()
             print("Exception")
         return ""
+
+    def gen_trading_info(self, stock, trading_type):
+        """
+            self.time = ""  # 또는 체결시간?
+            self.trading_type = ""  # [B]uy, [S]ell
+            self.code = ""  # 종목의 코드
+            self.stock_name = ""  # 종목 이름
+            self.stock_price = ""  # buy, sell 시 얼마에 사고팔았는지?
+            self.loss_flag = None  # 손해를 봤는지, 안봤는지 (buy 일땐 None, sell 일땐 True/False
+            self.profit_amount = 0  # 손해 또는 이익을 본 금액 (sell에서만 사용)
+            self.profit_rate = 0.0  # 손해 또는 이익을 본 비율 (sell에서만 사용)
+
+        :return:
+        """
+        tr = Trading(trading_type)
+
+        # 속성값이 겹치는 문제....
+        if trading_type == "B":  # BUY
+            stock_index = ['수익률']
+            account_index = ['수익률']
+        else:  # SELL
+            stock_index = ['수익률']
+            account_index = ['수익률']
+
+        tr = common.copy_attr(stock, tr, stock_index)
+        tr = common.copy_attr(self, tr, account_index)
+
+        return tr
+
+    def sell_transaction(self, stock):
+        """
+
+        :return:
+        """
+        tr = self.gen_trading_info(stock)
+        self.trading_history.record_sell_transaction(tr)
 
     @common.type_check
     def update_buy(self, stock: Stock, price_per_stock: int, amount: int, reason: str):
@@ -105,11 +146,20 @@ class Account(object):
         # self.총손익 --> Sell 할때 업데이트 됨
         self.보유주식[stock.code] = stock
 
+        self.buy_transaction(stock)
         try:
-            self.print_attr('buy')
-        except:
+            self.print_attr('buy')  # need to move to util/common pkg
+        except Exception:
             pdb.set_trace()
             print("Exception")
+
+    def buy_transaction(self, stock):
+        """
+
+        :return:
+        """
+        tr = self.gen_trading_info(stock)
+        self.trading_history.record_buy_transaction(tr)
 
     def revaluate(self):
         """주식가치가 변경(현재가 변경)되면서 계좌에 업데이트 해야 하는 정보를 모두 업데이트
@@ -139,6 +189,8 @@ class Account(object):
         :param timestamp:
         :return:
         """
+        self.timestamp = timestamp
+
         if not bool(self.보유주식):
             return
 
@@ -216,12 +268,13 @@ class Account(object):
         """
         return self.보유주식.values()
 
-    def clear_stock(self, stock, price):
+    def clear_stock(self, stock, price, reason):
         """보유한 주식중 특정 종목 일괄 청산
 
         :param code:
         :return:
         """
+        stock.trading_reason = reason
         stock.update_sell(int(price), int(stock.보유수량))
 
     def all_clear_stocks(self, timestamp):
@@ -229,22 +282,51 @@ class Account(object):
 
         :return:
         """
+        reason = "TRADING_TIME_EXPIRED"
         for stock in self.보유주식.values():
-            self.clear_stock(stock, stock.get_curr_price(timestamp))
+            self.clear_stock(stock, stock.get_curr_price(timestamp), reason)
 
+
+class Trading(object):
+    """Trading 된 시점의 모든 데이터를 snapshot 으로 보관하는 객체
+        특정시점의 모든 data를 snapshot뜨는 기능에 집중
+    """
+    def __init__(self, trading_type):
+        self.trading_type = trading_type
+        self.logger = TTlog().logger
 
 
 class TradingHistory(object):
-    def __init__(self):
-        self.time = ""  # 또는 체결시간?
-        self.trading_type = ""  # [B]uy, [S]ell
-        self.code = ""  # 종목의 코드
-        self.stock_name = ""  # 종목 이름
-        self.stock_price = ""  # buy, sell 시 얼마에 사고팔았는지?
-        self.loss_flag = None  # 손해를 봤는지, 안봤는지 (buy 일땐 None, sell 일땐 True/False
-        self.profit_amount = 0  # 손해 또는 이익을 본 금액 (sell에서만 사용)
-        self.profit_rate = 0.0  # 손해 또는 이익을 본 비율 (sell에서만 사용)
-        self.strategy_name = ""  # 사용한 전략명
-        self.condi_name = ""  # buy signal을 제공한 조건검색식(buy에서만 사용)
-        self.condi_index = ""  # buy signal을 제공한 조건검색식 index (buy에서만 사용)
+    """수많은 Trading 객체를 쉽게 조회, 가공, 분리, 재정렬 하는 기능을 제공하는 객체
+        이미 생성된 여러 Trading객체를 사용자 입맛에 맞게 가공하는데 집중
+        Trading관련 정보를 DB에 저장하는 임무
+    """
+    def __init__(self, strg_name, condi_index, condi_name):
+        self.strategy_name = strg_name  # 사용한 전략명
+        self.condi_name = condi_index  # buy signal을 제공한 조건검색식(buy에서만 사용)
+        self.condi_index = condi_name  # buy signal을 제공한 조건검색식 index (buy에서만 사용)
+        self.trading_history = []
         self.logger = TTlog().logger
+
+    def record_sell_transaction(self, trading_info):
+        """
+
+        :param trading_info:
+        :return:
+        """
+        self.trading_history.append(trading_info)
+
+    def record_buy_transaction(self, trading_info):
+        """
+
+        :param trading_info:
+        :return:
+        """
+        self.trading_history.append(trading_info)
+
+    def get_trading_history(self):
+        """
+
+        :return:
+        """
+        pass
--- util/common.py
+++ util/common.py
@@ -1,5 +1,6 @@
 from functools import wraps
 import pdb
+from util import constant
 
 
 def type_check(f):
@@ -24,3 +25,21 @@ def type_check(f):
         ret = f(*args, **kwargs)
         return ret
     return wrapper
+
+
+def copy_attr(from_o, to_o, attr_list):
+    """object간 속성복사를 수행
+
+    :param from_o: 속성값을 가져올(제공할) object
+    :param to_o: 속성값을 복사할 object
+    :param attr_list: 속성리스트
+    :return:
+    """
+
+    try:
+        for attr in attr_list:
+            val = from_o.__getattribute__(attr)
+            to_o.__setattr__(attr, val)
+    except Exception:
+        raise constant.CopyAttributeException('속성복사도중 예외 발생하였습니다.')
+    return to_o
